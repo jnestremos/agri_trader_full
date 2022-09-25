44c700028ae88e7a5be3aafc9e1d6daeb4b1bbb7
jnestremos <estremosjoshua@gmail.com>
2022-09-25 17:54:57 +0800

    added validation on supplier controller

--- agri-trader/app/Http/Controllers/SupplierController.php
+++ agri-trader/app/Http/Controllers/SupplierController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class SupplierController extends Controller
 {
-    //
+    public function addSupplier(Request $request){
+        $supplier = $request->validate([            
+            'supplier_name' => 'required',
+            'supplier_phoneNumber' => 'required',
+            'supplier_telNumber' => 'required',
+            'supplier_email' => 'required|email',
+            'address_zipcode' => 'required',
+            'address_street' => 'required',
+            'address_town' => 'required',
+            'address_province' => 'required',
+            'contact_firstName' => 'required',
+            'contact_middleName' => 'required',
+            'contact_lastName' => 'required',
+            'contact_suffix' => 'required',
+            'contact_position' => 'required',
+        ]);
+    }
 }
