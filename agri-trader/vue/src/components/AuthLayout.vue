42b467813d21ead2c2391d283742af073a13388f
EagleEye17 <rebotereymar@gmail.com>
2022-10-12 13:21:42 +0800

    f***** reports

--- agri-trader/vue/src/components/AuthLayout.vue
+++ agri-trader/vue/src/components/AuthLayout.vue
@@ -32,6 +32,7 @@
           <li v-if="role == 'trader'"><router-link style="color:black" to="/supplyInventory/dashboard">Supply Inventory </router-link></li>
           <li v-if="role == 'trader'"><router-link style="color:black" to="/supply/list"> Supply Profile </router-link></li>
           <li><router-link style="color:black" :to="role == 'farm_owner' ? `/reports/salesReport/owner`: ``">Sales Report</router-link></li>
+          <li><router-link style="color:black" to="/reports/dashboard">Reports Dashboard</router-link></li>
           <li>Profit Sharing Report</li>
         </ul>
       </div>
---
+++ agri-trader/vue/src/views/auth/Reports/AllReports.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="AllReports">
+    <div class="container-fluid w-100 d-flex pe-5 align-items-center" style="height:10%; background-color: #E0EDCA;">
+        <h3>Report Dashboard</h3>
+    </div>
+    <div class="container-fluid d-flex" style="height:90%; position: relative; z-index:9;">
+        <div class="container-fluid mt-5" style="width:100%; height: 30vh;">
+              <table id="Report Selection" class="table table-striped table-bordered align-middle" width="100%" style="margin: 0; border-collapse: collapse; border-spacing: 0cm;">
+                  <thead align="center">
+                      <tr>
+                          <router-link to="/income/summary"><th scope="col">Income Summary</th></router-link>
+                          <router-link to="/reports/ProduceReport"><th scope="col">Produce</th></router-link>
+                          <router-link to="/reports/ProfitSharing"><th scope="col">Profit Sharing</th></router-link>
+                          <router-link to="/reports/ReceivingReport"><th scope="col">Receiving Report from Supply Purchase Order</th></router-link>
+                          <router-link to="/reports/supplyExpenditures"><th scope="col">Supply Purchase Expense</th></router-link>
+                          <router-link to="/reports/spo"><th scope="col">Supply Purchase Order Reports</th></router-link>
+                          <router-link to="/reports/TraderSalesReport"><th scope="col">Trader Sales Report</th></router-link>
+                      </tr>
+                  </thead>
+            </table>
+         </div>   
+    </div>    
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+  export default {
+      name: "AllReports",
+      created() {
+          this.readyApp()
+      },
+      methods: {
+          ...mapActions(['readyApp'])
+      }
+  }
+</script>
+
+<style>
+
+</style>
